7b23d6e929e6dde4cfa8f409145267171710164f
vjvenkatesh <vjvenkatbsc@gmail.com>
2023-12-26 18:54:46 +0530

    changes in table name

--- src/main/java/com/example/demo/model/Employee.java
+++ src/main/java/com/example/demo/model/Employee.java
@@ -8,41 +8,46 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name= "employees")
+@Table(name = "employee")
 public class Employee {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
-	@Column(name="name")
+	@Column(name = "name")
 	private String name;
-	@Column(name="email")
+	@Column(name = "email")
 	private String email;
-	
-	
+
 	public Employee() {
-		
+
 	}
-	
+
 	public Employee(String name, String email) {
 		super();
 		this.name = name;
 		this.email = email;
 	}
+
 	public long getId() {
 		return id;
 	}
+
 	public void setId(long id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
